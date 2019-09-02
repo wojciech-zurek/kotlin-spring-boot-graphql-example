8140eafdfeb7ccc21fc7c38c4cc2ca732e03b6f9
Wojciech Zurek <w.zurek@i-bs.pl>
2019-09-02 14:24:48 +0200

    added simple crud

--- README.md
+++ README.md
@@ -38,3 +38,31 @@ The output will be:
     ]
   }
 }
+
+## Download
+
+```bash
+    git clone https://github.com/wojciech-zurek/kotlin-spring-fu-mongo-example.git
+```
+
+## Run with gradle
+
+```bash
+    cd kotlin-spring-fu-mongo-example/
+    ./gradlew run
+```
+
+## Run as jar file
+
+```bash
+    cd kotlin-spring-fu-mongo-example/
+    ./gradlew shadowJar
+    java -jar build/libs/kotlin-spring-fu-mongo-example-all.jar
+```
+
+## Test
+
+```bash
+    cd kotlin-spring-fu-mongo-example/
+    ./gradlew cleanTest test
+`````
--- src/main/kotlin/eu/wojciechzurek/example/ExampleApplication.kt
+++ src/main/kotlin/eu/wojciechzurek/example/ExampleApplication.kt
@@ -9,6 +9,7 @@ import org.springframework.context.support.beans
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -40,34 +41,61 @@ data class Book(
         @GeneratedValue(strategy = GenerationType.AUTO)
         val id: Long? = null,
         val publicId: String,
-        val title: String,
-        val author: String
+        var title: String,
+        var author: String
 )
 
 interface BookRepository : JpaRepository<Book, Long> {
     fun findByPublicId(publicId: String): Book
+    fun existsByPublicId(publicId: String): Boolean
+    fun deleteByPublicId(publicId: String)
 }
 
 interface BookService {
     fun getBook(publicId: String): Book
-    fun getBooks(count: Int): List<Book>
+    fun getBooks(size: Int): List<Book>
     fun createBook(title: String, author: String): Book
+    fun deleteBook(publicId: String): String?
+    fun updateBook(publicId: String, title: String, author: String): Book
 }
 
-class BookServiceImpl(private val bookRepository: BookRepository) : BookService {
+open class BookServiceImpl(private val bookRepository: BookRepository) : BookService {
+
+    @Transactional(readOnly = true)
     override fun getBook(publicId: String) = bookRepository.findByPublicId(publicId)
 
-    override fun getBooks(count: Int): List<Book> =
-            bookRepository.findAll(PageRequest.of(0, count, Sort.by(Sort.Direction.ASC, "id"))).content
+    @Transactional(readOnly = true)
+    override fun getBooks(size: Int): List<Book> =
+            bookRepository.findAll(PageRequest.of(0, size, Sort.by(Sort.Direction.ASC, "id"))).content
 
+    @Transactional
     override fun createBook(title: String, author: String) = bookRepository.save(Book(null, UUID.randomUUID().toString(), title, author))
+
+    @Transactional
+    override fun updateBook(publicId: String, title: String, author: String): Book {
+        val book = bookRepository.findByPublicId(publicId)
+        book.author = author
+        book.title = title
+        return bookRepository.save(book)
+    }
+
+    @Transactional
+    override fun deleteBook(publicId: String): String? {
+        if (bookRepository.existsByPublicId(publicId)) {
+            bookRepository.deleteByPublicId(publicId)
+            return publicId
+        }
+        return null
+    }
 }
 
 class BookQuery(private val bookService: BookService) : GraphQLQueryResolver {
     fun getBook(publicId: String) = bookService.getBook(publicId)
-    fun getBooks(count: Int) = bookService.getBooks(count)
+    fun getBooks(size: Int) = bookService.getBooks(size)
 }
 
 class BookMutation(private val bookService: BookService) : GraphQLMutationResolver {
     fun createBook(title: String, author: String) = bookService.createBook(title, author)
+    fun updateBook(publicId: String, title: String, author: String): Book = bookService.updateBook(publicId, title, author)
+    fun deleteBook(publicId: String): String? = bookService.deleteBook(publicId)
 }
